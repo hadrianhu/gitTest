9f7525256be354b5d0bd109e2a98700f0e53b92b
hadrianhu <ivanmworozi@gmail.com>
2014-08-17 20:06:12 +0300

    testnewActvity

--- TEST_Git/src/com/example/test_git/Moretest.java
+++ TEST_Git/src/com/example/test_git/Moretest.java
@@ -2,30 +2,20 @@ package com.example.test_git;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
-import android.util.Log;
 import android.view.Menu;
-import android.view.View;
 
-public class MainActivity extends Activity {
+public class Moretest extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//kk
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_moretest);
 	}
 
-	public void love(View v){
-		Log.d("love", "its working");
-		Intent i = new Intent(this,Moretest.class);
-		startActivity(i);
-	}
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.moretest, menu);
 		return true;
 	}
 
